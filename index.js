fb486b0660d73c5dc3820e355a7f137fbe02ca1c
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-04-13 00:08:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function driversWithRevenueOver(drivers, revenue){
   return drivers.filter(driver => driver.revenue > revenue)
 }
 
-function driverNamesWithRevenueOver(){
+function driverNamesWithRevenueOver(drivers, revenue){
   
 }
